6c26ae93aa92d7c19f5b5e2e748dfc8aa53139c3
BehzadDara <behzad.dara.99@gmail.com>
2023-06-14 10:36:18 +0330

    .

--- LINQ2/Program.cs
+++ LINQ2/Program.cs
@@ -15,3 +15,22 @@ var sample31 = new int[] { 0, 5, 9};
 var sample32 = new int[] { 1, 7, 8};
 Console.WriteLine($"two select together: {string.Join(", ", sample31.SelectMany(a => sample32.Where(b => a < b), (a, b) => (a, b)))}");
 // (0, 1), (0, 7), (0, 8), (5, 7), (5, 8)
+
+// Eager and Lazy SELECT
+var sample41 = new int[] { 5, 4, 1, 2, 8, 6, 7, 2, 0 };
+var i1 = 0;
+var q1 = sample41.Select(n => ++i1); // lazy
+Console.WriteLine("print i1 without ToList:");
+foreach (var v1 in q1)
+{
+    Console.WriteLine($"v1 = {v1}, i1 = {i1}");
+}
+var i2 = 0;
+var q2 = sample41.Select(n => ++i2).ToList(); // eager
+Console.WriteLine("print i2 with ToList:");
+foreach (var v2 in q2)
+{
+    Console.WriteLine($"v2 = {v2}, i2 = {i2}");
+}
+// 1,2,3,4 ... // 9,9,9,9 ...
+
